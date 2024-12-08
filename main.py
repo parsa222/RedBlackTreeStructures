3debc8e3027ad5586b80cf028e8d9e635db482a0
parsa222 <70150489+parsa222@users.noreply.github.com>
2024-12-08 11:48:07 -0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,5 @@
+# If you're seeing this, remind me to upload the actual code. 
+# I might have been busy and totally forgotten about this repo. 
+
+
+# /Desktop/scripts/rbtree3.2 or could be 3.3, idk i have different versions xD i should start using Git locally 
